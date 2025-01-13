5b7d2ec22fc612b3249079532e27ad4b25e6235b
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-13 14:43:04 -0300

    feat: create SearchScreen boilerplate

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+      <SearchBar />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
