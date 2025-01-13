6952d4d12780d6ae1c8a8ccd7e320d6f877ab87f
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-13 17:39:04 -0300

    create helper function searchApi, add useEffect to call searchApi on first render

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+      setErrorMessage("");
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
   return (
     <View>
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => {
-          console.log("term was submitted");
-        }}
-      />
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => searchApi(term)} />
+      <Text>we have found {results.length} results</Text>
+      {errorMessage && <Text>{errorMessage}</Text>}
     </View>
   );
 };
