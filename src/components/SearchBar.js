43e5ee95b7b0bd10fc5ee45626374cb42477b499
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-13 16:26:13 -0300

    feat: add new state term, add props to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.background}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
