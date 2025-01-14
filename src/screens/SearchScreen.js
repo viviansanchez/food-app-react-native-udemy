cd3b9f14f2ff6c7af19052e1b082b2a5682e5210
Vivian Sanchez Desktop <visasal88@gmail.com>
2025-01-14 13:03:03 -0300

    feat: add filterResultsByPrice function, add ResultsList components

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import ResultsList from "../components/ResultsList";
 import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = (price) => {
+    //price === '$' || '$$' || '$$$'
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
   return (
     <View>
       <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} onTermSubmit={() => searchApi(term)} />
       <Text>we have found {results.length} results</Text>
       {errorMessage && <Text>{errorMessage}</Text>}
+      <ResultsList title={"Cost Effective"} results={filterResultsByPrice("$")} />
+      <ResultsList title={"Bit Pricier"} results={filterResultsByPrice("$$")} />
+      <ResultsList title={"Big Spender"} results={filterResultsByPrice("$$$")} />
     </View>
   );
 };
